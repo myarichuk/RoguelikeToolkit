c627bb3534e7e012dfcf8a6dda7c589fc7c9a560
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-01-06 09:29:49 +0200

    Minor refactorings

--- RoguelikeToolkit.Entities/EntityFactory.cs
+++ RoguelikeToolkit.Entities/EntityFactory.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using AutoMapper;
 using DefaultEcs;
