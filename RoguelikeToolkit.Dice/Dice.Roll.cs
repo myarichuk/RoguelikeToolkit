0453e2a45eb469508d4690e5517e8a36be29859e
Michael Yarichuk <michael.yarichuk@gmail.com>
2021-08-26 20:48:09 +0300

    minor refactoring for dice

--- RoguelikeToolkit.Dice/Dice.Roll.cs
+++ RoguelikeToolkit.Dice/Dice.Roll.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
 using Troschuetz.Random;
 using Troschuetz.Random.Generators;
 
@@ -8,7 +8,8 @@ namespace RoguelikeToolkit.DiceExpression
 {
     public partial class Dice
     {
-        private static readonly IGenerator Random = new NR3Q1Generator();
+        //NR3Q1Generator is not thread-safe
+        private static readonly ThreadLocal<IGenerator> Random = new ThreadLocal<IGenerator>(() => new NR3Q1Generator());
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static int Roll100() => RollAndSum(1, 100);
@@ -17,16 +18,14 @@ namespace RoguelikeToolkit.DiceExpression
         {
             var result = 0;
             for (int i = 0; i < numOfDice; i++)
-                lock (Random) //NR3Q1Generator is not thread-safe
-                    result += Random.Next(1, sides);
+                result += Random.Value.Next(1, sides);
             return result;
         }
 
         internal static IEnumerable<int> Roll(int numOfDice, int sides)
         {
             for (int i = 0; i < numOfDice; i++)
-                lock (Random) //NR3Q1Generator is not thread-safe
-                    yield return Random.Next(1, sides);
+                yield return Random.Value.Next(1, sides);
         }
 
     }
