ad03d1875a54f665c852a9defca930353c6f201c
Michael Yarichuk <michael.yarichuk@gmail.com>
2021-08-28 10:12:23 +0300

    make sure object pool is threadsafe + some more tests

--- RoguelikeToolkit.Entities.Tests/InheritanceGraphTests.cs
+++ RoguelikeToolkit.Entities.Tests/InheritanceGraphTests.cs
@@ -13,8 +13,8 @@ namespace RoguelikeToolkit.Entities.Tests
             var childA = EntityTemplate.ParseFromString(B);
             var childB = EntityTemplate.ParseFromString(C);
 
-            var graph = new InheritanceGraph(new []{ root, childB, childA });
-            
+            var graph = new InheritanceGraph(new[] { root, childB, childA });
+
             var inheritanceChain = graph.GetInheritanceChainFor(childB).ToList();
 
             Assert.Equal(3, inheritanceChain.Count); //sanity check
@@ -26,7 +26,51 @@ namespace RoguelikeToolkit.Entities.Tests
         }
 
         [Fact]
-        public void Inheritance_chain_with_cycles_should_throw()
+        public void Multiple_Inheritance_chain_should_keep_inheritance_order()
+        {
+            var t1 = EntityTemplate.ParseFromString(T1);
+            var t2 = EntityTemplate.ParseFromString(T2);
+            var t3 = EntityTemplate.ParseFromString(T3);
+            var t4 = EntityTemplate.ParseFromString(T4);
+
+            var graph = new InheritanceGraph(new[] { t1, t3, t2, t4 });
+
+            var inheritanceChain = graph.GetInheritanceChainFor(t4).ToList();
+
+            Assert.Equal(4, inheritanceChain.Count); //sanity check
+
+            //multiple inheritance will treat parents of the same level in-order
+            Assert.Equal(t4.Id, inheritanceChain[0].Id);
+            Assert.Equal(t1.Id, inheritanceChain[1].Id);
+            Assert.Equal(t2.Id, inheritanceChain[2].Id);
+            Assert.Equal(t3.Id, inheritanceChain[3].Id);
+        }
+
+        [Fact]
+        public void Multiple_Inheritance_chain_multiple_levels_should_keep_inheritance_order()
+        {
+            var t1 = EntityTemplate.ParseFromString(T1);
+            var t2 = EntityTemplate.ParseFromString(T2);
+            var t3 = EntityTemplate.ParseFromString(T3);
+            var t4 = EntityTemplate.ParseFromString(T4);
+            var t5 = EntityTemplate.ParseFromString(T5);
+
+            var graph = new InheritanceGraph(new[] { t1, t3, t2, t4, t5 });
+
+            var inheritanceChain = graph.GetInheritanceChainFor(t5).ToList();
+
+            Assert.Equal(5, inheritanceChain.Count); //sanity check
+
+            //multiple inheritance will treat parents of the same level in-order
+            Assert.Equal(t5.Id, inheritanceChain[0].Id);
+            Assert.Equal(t4.Id, inheritanceChain[1].Id);
+            Assert.Equal(t1.Id, inheritanceChain[2].Id);
+            Assert.Equal(t2.Id, inheritanceChain[3].Id);
+            Assert.Equal(t3.Id, inheritanceChain[4].Id);
+        }
+
+        [Fact]
+        public void Inheritance_chain_with_cycles_should_traverse_cycle_only_once()
         {
             var a = EntityTemplate.ParseFromString(A); //we need *some* roots to pass sanity check validation
             var x1 = EntityTemplate.ParseFromString(X1);
@@ -35,7 +79,19 @@ namespace RoguelikeToolkit.Entities.Tests
 
             var graph = new InheritanceGraph(new[] { x1, x2, x3, a });
 
-            Assert.Throws<InvalidOperationException>(() => graph.GetInheritanceChainFor(x3).ToList());
+            var inheritanceChain = graph.GetInheritanceChainFor(x3).ToList();
+
+            Assert.Equal(x3.Id, inheritanceChain[0].Id);
+            Assert.Equal(x2.Id, inheritanceChain[1].Id);
+            Assert.Equal(x1.Id, inheritanceChain[2].Id);
+
+        }
+
+        [Fact]
+        public void Non_existent_inheritance_Id_should_throw()
+        {
+            var x2 = EntityTemplate.ParseFromString(X2);
+            Assert.Throws<InvalidOperationException>(() => _ = new InheritanceGraph(new[] { x2 }));
         }
 
         #region Simple Hierarchy Templates
@@ -62,6 +118,50 @@ namespace RoguelikeToolkit.Entities.Tests
                 ""Inherits"": [""B""]
             }
         ";
+        #endregion
+
+        #region Multiple Inheritance Template
+
+        private const string T1 =
+        @"
+            {
+                ""Id"":""T1"",
+                ""Inherits"": []
+            }
+        ";
+
+        private const string T2 =
+        @"
+            {
+                ""Id"":""T2"",
+                ""Inherits"": []
+            }
+        ";
+
+        private const string T3 =
+        @"
+            {
+                ""Id"":""T3"",
+                ""Inherits"": []
+            }
+        ";
+
+        private const string T4 =
+        @"
+            {
+                ""Id"":""T4"",
+                ""Inherits"": [""T1"", ""T2"", ""T3""]
+            }
+        ";
+
+        private const string T5 =
+@"
+            {
+                ""Id"":""T5"",
+                ""Inherits"": [""T4"", ""T1""]
+            }
+        ";
+
         #endregion
 
         #region Inheritance Cycle Templates
--- RoguelikeToolkit.Entities/Entities/InheritanceGraph.cs
+++ RoguelikeToolkit.Entities/Entities/InheritanceGraph.cs
@@ -8,8 +8,8 @@ namespace RoguelikeToolkit.Entities
 {
     public class InheritanceGraph
     {
-        private readonly static ObjectPool<Queue<string>> _queuePool = ObjectPoolProvider.Instance.Create<Queue<string>>();
-        private readonly static ObjectPool<HashSet<string>> _visitedPool = ObjectPoolProvider.Instance.Create<HashSet<string>>();
+        private readonly static ObjectPool<Queue<string>> _queuePool = ObjectPoolProvider.Instance.Create(new ThreadSafeObjectPoolPolicy<Queue<string>>());
+        private readonly static ObjectPool<HashSet<string>> _visitedPool = ObjectPoolProvider.Instance.Create(new ThreadSafeObjectPoolPolicy<HashSet<string>>());
 
         private readonly IReadOnlyDictionary<string, List<string>> _adjacencyList;
         private readonly IReadOnlyDictionary<string, EntityTemplate> _templates;
@@ -22,10 +22,9 @@ namespace RoguelikeToolkit.Entities
         {
             ValidateAndThrowIfNeeded(templates);
 
-            //index inheritance
-            _adjacencyList = templates.ToDictionary(
-                t => t.Id,
-                t => t.Inherits.ToList());
+            _adjacencyList = templates
+                .ToDictionary(t => t.Id,
+                              t => t.Inherits.ToList());
 
             _templates = templates.ToDictionary(t => t.Id, t => t);
         }
@@ -33,12 +32,12 @@ namespace RoguelikeToolkit.Entities
         //in order to calculate "effective" template, we need to traverse up the inheritance chain
         public IEnumerable<EntityTemplate> GetInheritanceChainFor(EntityTemplate template)
         {
-            if (template?.Inherits.Count == 0) //no need to work hard...
-                yield break;
-
             //sanity check, not strictly needed but it is good this is here
             if (_templates.ContainsKey(template.Id) == false)
                 ThrowIrrelevantTemplate(template);
+
+            if (template?.Inherits.Count == 0) //no need to work hard...
+                yield break;
 
             Queue<string> traversalQueue = null;
             HashSet<string> visited = null;
@@ -46,23 +45,19 @@ namespace RoguelikeToolkit.Entities
             {
                 traversalQueue = _queuePool.Get();
                 visited = _visitedPool.Get();
-
                 traversalQueue.Enqueue(template.Id);
                 while (traversalQueue.Count > 0)
                 {
                     var currentId = traversalQueue.Dequeue();
 
-                    if (visited.Contains(currentId))
-                        throw new InvalidOperationException("Inheritance circle detected, it is not allowed to have entity templates inheritance circles");
+                    if (visited.Contains(currentId)) //ignore circles
+                        continue;
 
                     visited.Add(currentId);
                     yield return _templates[currentId];
 
                     for (int i = 0; i < _adjacencyList[currentId].Count; i++)
-                    {
-                        var adjacentId = _adjacencyList[currentId][i];
-                        traversalQueue.Enqueue(adjacentId);
-                    }
+                        traversalQueue.Enqueue(_adjacencyList[currentId][i]);
                 }
             }
             finally
@@ -91,7 +86,12 @@ namespace RoguelikeToolkit.Entities
         {
             //sanity check, this shouldn't happen!
             if (templates.Any(t => t.Inherits.Contains(t.Id)))
-                throw new InvalidOperationException($"Self-inheritance is now allowed as it is silly. (self-inheritance found in tempalte with Id = {templates.First(t => t.Inherits.Contains(t.Id)).Id}");
+                throw new InvalidOperationException($"Self-inheritance is now allowed as it is silly. (self-inheritance found in template with Id = {templates.First(t => t.Inherits.Contains(t.Id)).Id}");
+
+            foreach(var template in templates)
+                foreach (var inheritsId in template.Inherits)
+                    if (templates.Any(x => x.Id == inheritsId) == false)
+                        throw new InvalidOperationException($"Template (id = {template.Id}) contains invalid (non-existent) Id in 'Inherits' field {inheritsId}.");
         }
 
         #endregion
