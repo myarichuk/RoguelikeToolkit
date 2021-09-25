7198f9c4e3ab7c225b7ea219d2a707b52126acb5
Michael Yarichuk <michael.yarichuk@gmail.com>
2021-09-25 20:09:16 +0300

    remove irrelevant commented code

--- RoguelikeToolkit.Scripts/EntityInteractionScript.cs
+++ RoguelikeToolkit.Scripts/EntityInteractionScript.cs
@@ -16,30 +16,3 @@ namespace RoguelikeToolkit.Scripts
             _script.ExecuteOn(ref source, "source", ref target, "target", @params);
     }
 }
-
-
-//using DefaultEcs;
-
-//namespace RoguelikeToolkit.Scripts
-//{
-//    public class EntityInteractionScript
-//    {
-//        private readonly InteractionScript _script;
-
-//        public EntityInteractionScript(string script, string sourceInstanceName = null, string targetInstanceName = null) =>
-//            _script = new InteractionScript(script, sourceInstanceName, targetInstanceName);
-
-//        public bool TryExecuteOn<TSourceComponent, TTargetComponent>(in Entity sourceEntity, in Entity targetEntity)
-//        {
-//            if (!sourceEntity.Has<TSourceComponent>() ||
-//                !targetEntity.Has<TTargetComponent>())
-//                    return false;
-
-//            ref var source = ref sourceEntity.Get<TSourceComponent>();
-//            ref var target = ref targetEntity.Get<TTargetComponent>();
-
-//            _script.ExecuteOn(ref source, ref target);
-//            return true;
-//        }
-//    }
-//}
