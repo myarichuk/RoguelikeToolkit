0cee1cf088b37ae037248954745e664791fa6de2
Michael Yarichuk <michael.yarichuk@gmail.com>
2022-03-26 08:42:22 +0300

    add methods for removal of key combinations

--- RoguelikeToolkit.Keyboard/KeyMappingStore.cs
+++ RoguelikeToolkit.Keyboard/KeyMappingStore.cs
@@ -7,6 +7,10 @@ using Utf8Json.Resolvers;
 
 namespace RoguelikeToolkit.Keyboard
 {
+	public class KeyMappingStore<KeyEnum> : KeyMappingStore<KeyEnum, string> where KeyEnum : Enum
+	{
+	}
+
 	//naive implementation, but shouldn't be a problem since there will be a few shortcuts..
 	//should be optimized if it proves to be a bottleneck
 	public class KeyMappingStore<KeyEnum,TData> where KeyEnum : Enum
@@ -19,6 +23,11 @@ namespace RoguelikeToolkit.Keyboard
 		internal KeyMappingStore(List<KeyMapping<KeyEnum, TData>> keyCombinations) =>
 			_keyCombinations = keyCombinations;
 
+		/// <summary>
+		/// Load key mappings from a JSON file
+		/// </summary>
+		/// <param name="filename">Path of the JSON file</param>
+		/// <returns>Key mappings repository</returns>
 		public static KeyMappingStore<KeyEnum, TData> LoadFrom(string filename)
 		{
 			using var fs = File.OpenRead(filename);
@@ -27,6 +36,10 @@ namespace RoguelikeToolkit.Keyboard
 			return new KeyMappingStore<KeyEnum, TData>(keyCombinations);
 		}
 
+		/// <summary>
+		/// Serialize key mappings into JSON file
+		/// </summary>
+		/// <param name="filename">Path of the JSON file</param>
 		public void SaveTo(string filename)
 		{
 			using var fs = File.Create(filename);
@@ -34,17 +47,38 @@ namespace RoguelikeToolkit.Keyboard
 			fs.Flush();
 		}
 
+		/// <summary>
+		/// Fetch action IDs that map to a specified key
+		/// </summary>
+		/// <param name="key">Key mapping</param>
+		/// <remarks>may return duplicates as multiple key combinations can map to the same action</remarks>
+		/// <returns>IEnumerable of action IDs</returns>
 		public IEnumerable<TData> FetchActionIDsFor(KeyEnum key) =>
 			_keyCombinations.Where(kc => kc.FirstKey.Equals(key))
 							.Select(kc => kc.Action)
 							.Distinct();
 
+		/// <summary>
+		/// Fetch action IDs that map to a specified key combination
+		/// </summary>
+		/// <param name="key1">Key mapping 1</param>
+		/// <param name="key2">Key mapping 2</param>
+		/// <remarks>may return duplicates as multiple key combinations can map to the same action</remarks>
+		/// <returns>IEnumerable of action IDs</returns>
 		public IEnumerable<TData> FetchActionIDsFor(KeyEnum key1, KeyEnum key2) =>
 			_keyCombinations.Where(kc => kc.FirstKey.Equals(key1) &&
 															  kc.SecondKey.Equals(key2))
 							.Select(kc => kc.Action)
 							.Distinct();
 
+		/// <summary>
+		/// Fetch action IDs that map to a specified key combination
+		/// </summary>
+		/// <param name="key1">Key mapping 1</param>
+		/// <param name="key2">Key mapping 2</param>
+		/// <param name="key3">Key mapping 3</param>
+		/// <remarks>may return duplicates as multiple key combinations can map to the same action</remarks>
+		/// <returns>IEnumerable of action IDs</returns>
 		public IEnumerable<TData> FetchActionIDsFor(KeyEnum key1, KeyEnum key2, KeyEnum key3) =>
 			_keyCombinations.Where(kc => kc.FirstKey.Equals(key1) &&
 															  kc.SecondKey.Equals(key2) &&
@@ -52,19 +86,32 @@ namespace RoguelikeToolkit.Keyboard
 							.Select(kc => kc.Action)
 							.Distinct();
 
+		/// <summary>
+		/// Checks whether there are any mappings for a specified key
+		/// </summary>
 		public bool HasMappingFor(KeyEnum key) =>
 			_keyCombinations.Any(kc =>
 				kc.FirstKey.Equals(key) && kc.SecondKey.Equals(DefaultValue) && kc.ThirdKey.Equals(DefaultValue));
 
+		/// <summary>
+		/// Checks whether there are any mappings for a specified key combination
+		/// </summary>
 		public bool HasMappingFor(KeyEnum key1, KeyEnum key2) =>
 			_keyCombinations.Any(kc =>
 				kc.FirstKey.Equals(key1) && kc.SecondKey.Equals(key2) && kc.ThirdKey.Equals(DefaultValue));
 
+		/// <summary>
+		/// Checks whether there are any mappings for a specified key combination
+		/// </summary>
 		public bool HasMappingFor(KeyEnum key1, KeyEnum key2, KeyEnum key3) =>
 			_keyCombinations.Any(kc =>
 				kc.FirstKey.Equals(key1) && kc.SecondKey.Equals(key2) && kc.ThirdKey.Equals(key3));
 
-
+		/// <summary>
+		/// Add key mapping
+		/// </summary>
+		/// <param name="key">key enum</param>
+		/// <param name="actionId">action id</param>
 		public void AddMapping(KeyEnum key, in TData actionId) =>
 			_keyCombinations.Add(new KeyMapping<KeyEnum, TData>
 			{
@@ -72,6 +119,12 @@ namespace RoguelikeToolkit.Keyboard
 				Action = actionId
 			});
 
+		/// <summary>
+		/// Add key mapping
+		/// </summary>
+		/// <param name="key1">first key enum</param>
+		/// <param name="key2">second key enum</param>
+		/// <param name="actionId">action id</param>
 		public void AddMapping(KeyEnum key1, KeyEnum key2, in TData actionId) =>
 			_keyCombinations.Add(new KeyMapping<KeyEnum, TData>
 			{
@@ -80,6 +133,13 @@ namespace RoguelikeToolkit.Keyboard
 				Action = actionId
 			});
 
+		/// <summary>
+		/// Add key mapping
+		/// </summary>
+		/// <param name="key1">first key enum</param>
+		/// <param name="key2">second key enum</param>
+		/// <param name="key3">third key enum</param>
+		/// <param name="actionId">action id</param>
 		public void AddMapping(KeyEnum key1, KeyEnum key2, KeyEnum key3, in TData actionId) =>
 			_keyCombinations.Add(new KeyMapping<KeyEnum, TData>
 			{
@@ -88,5 +148,44 @@ namespace RoguelikeToolkit.Keyboard
 				ThirdKey = key3,
 				Action = actionId
 			});
+
+		/// <summary>
+		/// Remove all key combinations that match the specified key
+		/// </summary>
+		/// <remarks>note that the two other keys must not be set for the combination to be deleted</remarks>
+		/// <param name="key">Key to match</param>
+		/// <returns>Number of key combinations to be removed</returns>
+		public int RemoveMappingOf(KeyEnum key) =>
+			_keyCombinations.RemoveAll(
+				kc => kc.FirstKey.Equals(key) &&
+					kc.SecondKey.Equals(DefaultValue) &&
+					kc.ThirdKey.Equals(DefaultValue));
+
+		/// <summary>
+		/// Remove all key combinations that match the specified key
+		/// </summary>
+		/// <remarks>note that the third key must not be set for the combination to be deleted</remarks>
+		/// <param name="key1">Key 1 to match</param>
+		/// <param name="key2">Key 2 to match</param>
+		/// <returns>Number of key combinations to be removed</returns>
+		public int RemoveMappingOf(KeyEnum key1, KeyEnum key2) =>
+			_keyCombinations.RemoveAll(
+				kc => kc.FirstKey.Equals(key1) &&
+					kc.SecondKey.Equals(key2) &&
+					kc.ThirdKey.Equals(DefaultValue));
+
+		/// <summary>
+		/// Remove all key combinations that match the specified key
+		/// </summary>
+		/// <remarks>note that the third key must not be set for the combination to be deleted</remarks>
+		/// <param name="key1">Key 1 to match</param>
+		/// <param name="key2">Key 2 to match</param>
+		/// <param name="key3">Key 3 to match</param>
+		/// <returns>Number of key combinations to be removed</returns>
+		public int RemoveMappingOf(KeyEnum key1, KeyEnum key2, KeyEnum key3) =>
+			_keyCombinations.RemoveAll(
+				kc => kc.FirstKey.Equals(key1) &&
+					kc.SecondKey.Equals(key2) &&
+					kc.ThirdKey.Equals(key3));
 	}
 }
