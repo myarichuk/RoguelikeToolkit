8f0915570502adcb1af10463675b06c01d2ba1db
Michael Yarichuk <michael.yarichuk@gmail.com>
2022-04-01 22:33:35 +0300

    adjust versions and make sure stuff compiles & tests pass

--- RoguelikeToolkit.Dice/Dice.Roll.cs
+++ RoguelikeToolkit.Dice/Dice.Roll.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections.Generic;
+using RandN;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using Troschuetz.Random;
-using Troschuetz.Random.Generators;
+using RandN.Compat;
 
 namespace RoguelikeToolkit.DiceExpression
 {
     public partial class Dice
     {
         //NR3Q1Generator is not thread-safe
-        private static readonly ThreadLocal<IGenerator> Random = new ThreadLocal<IGenerator>(() => new NR3Q1Generator());
+        private static readonly ThreadLocal<RandomShim<SmallRng>> Random = new ThreadLocal<RandomShim<SmallRng>>(() => RandomShim.Create(SmallRng.Create()));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static int Roll100() => RollAndSum(1, 100);
 
         internal static int RollAndSum(int numOfDice, int sides)
         {
-            var result = 0;
-            for (int i = 0; i < numOfDice; i++)
+            var result = 0;			
+
+			for (int i = 0; i < numOfDice; i++)
             {
                 result += Random.Value.Next(1, sides);
             }
--- RoguelikeToolkit.Tryouts/Program.cs
+++ RoguelikeToolkit.Tryouts/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using RoguelikeToolkit.DiceExpression;
 using Syllabore;
@@ -9,77 +9,77 @@ namespace RoguelikeToolkit.Tryouts
     {
         private static void Main(string[] args)
         {
-            var provider = new DefaultSyllableProvider();
-            var validator = new NameValidator()
-                    .DoNotAllowPattern(@"[j|p|q|w]$")             // Invalidate these awkward endings
-                    .DoNotAllowPattern(@"(\w)\1\1")               // Invalidate any sequence of 3 or more identical letters
-                    .DoNotAllowPattern(@"([^aeiouAEIOU])\1\1\1"); // Invalidate any sequence of 4 or more consonants
+            //var provider = new DefaultSyllableProvider();
+            //var validator = new NameValidator()
+            //        .DoNotAllowPattern(@"[j|p|q|w]$")             // Invalidate these awkward endings
+            //        .DoNotAllowPattern(@"(\w)\1\1")               // Invalidate any sequence of 3 or more identical letters
+            //        .DoNotAllowPattern(@"([^aeiouAEIOU])\1\1\1"); // Invalidate any sequence of 4 or more consonants
 
-            var g = new NameGenerator(provider, validator);
+            //var g = new NameGenerator(provider, validator);
 
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(g.Next());
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(g.Next());
+            //}
 
-            Console.WriteLine("---");
+            //Console.WriteLine("---");
 
-            // You can choose to build name generators programmatically.
-            g = new NameGenerator()
-                .UsingProvider(x => x
-                    .WithLeadingConsonants("str")
-                    .WithVowels("ae"))
-                .LimitSyllableCount(3);
+            //// You can choose to build name generators programmatically.
+            //g = new NameGenerator()
+            //    .UsingProvider(x => x
+            //        .WithLeadingConsonants("str")
+            //        .WithVowels("ae"))
+            //    .LimitSyllableCount(3);
 
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(g.Next());
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(g.Next());
+            //}
 
-            Console.WriteLine("---");
+            //Console.WriteLine("---");
 
-            g = new NameGenerator().UsingMutator(new VowelMutator());
+            //g = new NameGenerator().UsingMutator(new VowelMutator());
 
-            for (int i = 0; i < 3; i++)
-            {
-                var name = g.NextName();
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    var name = g.NextName();
 
-                Console.WriteLine(name);
+            //    Console.WriteLine(name);
 
-                for (int j = 0; j < 4; j++)
-                {
-                    var variation = g.Mutator.Mutate(name);
-                    Console.WriteLine(variation);
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        var variation = g.Mutator.Mutate(name);
+            //        Console.WriteLine(variation);
 
-                }
-            }
+            //    }
+            //}
 
-            Console.WriteLine("---");
-            g = new NameGenerator()
-                                .UsingProvider(p => p
-                                    .WithVowels("aeoy")
-                                    .WithLeadingConsonants("vstlr")
-                                    .WithTrailingConsonants("zrt")
-                                    .WithVowelSequences("ey", "ay", "oy"))
-                                .UsingValidator(v => v
-                                    .DoNotAllowPattern(
-                                        @".{12,}",
-                                        @"(\w)\1\1",             // Prevents any letter from occuring three times in a row
-                                        @".*([y|Y]).*([y|Y]).*", // Prevents double y
-                                        @".*([z|Z]).*([z|Z]).*", // Prevents double z
-                                        @"(zs)",                 // Prevents "zs"
-                                        @"(y[v|t])"))            // Prevents "yv" and "yt"
-                                .LimitMutationChance(0.99)
-                                .LimitSyllableCount(2, 4);
+            //Console.WriteLine("---");
+            //g = new NameGenerator()
+            //                    .UsingProvider(p => p
+            //                        .WithVowels("aeoy")
+            //                        .WithLeadingConsonants("vstlr")
+            //                        .WithTrailingConsonants("zrt")
+            //                        .WithVowelSequences("ey", "ay", "oy"))
+            //                    .UsingValidator(v => v
+            //                        .DoNotAllowPattern(
+            //                            @".{12,}",
+            //                            @"(\w)\1\1",             // Prevents any letter from occuring three times in a row
+            //                            @".*([y|Y]).*([y|Y]).*", // Prevents double y
+            //                            @".*([z|Z]).*([z|Z]).*", // Prevents double z
+            //                            @"(zs)",                 // Prevents "zs"
+            //                            @"(y[v|t])"))            // Prevents "yv" and "yt"
+            //                    .LimitMutationChance(0.99)
+            //                    .LimitSyllableCount(2, 4);
 
-            ConfigurationFile.Save(g, "city-name-generator.txt");
-            var g2 = ConfigurationFile.Load("city-name-generator.txt");
+            //ConfigurationFile.Save(g, "city-name-generator.txt");
+            //var g2 = ConfigurationFile.Load("city-name-generator.txt");
 
-            for (int i = 0; i < 50; i++)
-            {
-                var name = g.NextName();
-                Console.WriteLine(name);
-            }
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    var name = g.NextName();
+            //    Console.WriteLine(name);
+            //}
 
         }
     }
