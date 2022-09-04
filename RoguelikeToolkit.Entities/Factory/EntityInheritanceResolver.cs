e39cd40c35272665e423972b09b1faf7bf381b1b
Michael Yarichuk <michael.yarichuk@gmail.com>
2022-09-04 11:58:41 +0300

    minor

--- RoguelikeToolkit.Entities/Factory/EntityInheritanceResolver.cs
+++ RoguelikeToolkit.Entities/Factory/EntityInheritanceResolver.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace RoguelikeToolkit.Entities.Factory
 {
@@ -12,26 +13,35 @@ namespace RoguelikeToolkit.Entities.Factory
 			_tryGetByName = getByIdFunc;
 
 		//traverse the components inheritance and merge all the components
-		public EntityTemplate GetEffectiveTemplate(EntityTemplate flatTemplate)
+		public EntityTemplate GetEffectiveTemplate(EntityTemplate flatTemplate, bool throwOnMissingInheritance = true)
 		{
 			//note: this executes copy constructor (feature of C# records!)
 			//note 2: if no copy constructor present, this will create shallow clone (inner properties would be the same)
+			Debug.Assert(flatTemplate != null, nameof(flatTemplate) + " != null");
+
 			var templateCopy = flatTemplate with { };
 
 			foreach (var inheritedTemplateName in flatTemplate.Inherits)
 			{
-				if (_tryGetByName(inheritedTemplateName, out var fetchedTemplate))
+				if (_tryGetByName(inheritedTemplateName, out var inheritedTemplate))
 				{
-					var embeddedEffectiveTemplate = GetEffectiveTemplate(fetchedTemplate);
+					var embeddedEffectiveTemplate = GetEffectiveTemplate(inheritedTemplate);
 					templateCopy.Components.MergeWith(embeddedEffectiveTemplate.Components); //no key overrides!
 					templateCopy.Tags.UnionWith(embeddedEffectiveTemplate.Tags);
 					templateCopy.Inherits.UnionWith(embeddedEffectiveTemplate.Inherits);
 				}
 				else
 				{
-					throw new InvalidOperationException(
-						$"Inherited template name '{inheritedTemplateName}' in  not found. " +
-									$"(check template with name = '{flatTemplate.Name}')");
+					if (throwOnMissingInheritance)
+					{
+						throw new InvalidOperationException(
+							$"Inherited template name '{inheritedTemplateName}' in  not found. " +
+							$"(check template with name = '{flatTemplate.Name}')");
+					}
+					else
+					{
+						//TODO: add logging in this case
+					}
 				}
 			}
 
