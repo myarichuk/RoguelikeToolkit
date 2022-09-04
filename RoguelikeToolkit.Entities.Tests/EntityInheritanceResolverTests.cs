195f63d12dd6b69cb039128e9c22ada028c5da50
Michael Yarichuk <michael.yarichuk@gmail.com>
2022-09-04 11:45:13 +0300

    another test

--- RoguelikeToolkit.Entities.Tests/EntityInheritanceResolverTests.cs
+++ RoguelikeToolkit.Entities.Tests/EntityInheritanceResolverTests.cs
@@ -26,22 +26,60 @@ namespace RoguelikeToolkit.Entities.Tests
 		}
 
 		[Fact]
+		public void Can_resolve_without_inheritance()
+		{
+			//sanity check
+			Assert.True(_repository.TryGetByName("template-no-inheritance", out var template));
+			var templateWithInheritanceResolved = _inheritanceResolver.GetEffectiveTemplate(template);
+
+			Assert.Collection(templateWithInheritanceResolved.Tags,
+				item => Assert.Equal("tag1", item),
+				item => Assert.Equal("tag2", item),
+				item => Assert.Equal("tag3", item));
+
+			Assert.Collection(templateWithInheritanceResolved.Components,
+				kvp =>
+				{
+					Assert.Equal("foobar", kvp.Key);
+
+					//embedded objects yaml deserializer loads as Dictionary<object, object>
+					var valueAsDict = (Dictionary<object, object>)kvp.Value;
+					Assert.Equal("abcdef", valueAsDict["stringProperty"]);
+					Assert.Equal((byte)123, valueAsDict["numProperty"]);
+				},
+				kvp =>
+				{
+					Assert.Equal("barfoo", kvp.Key);
+					var valueAsDict = (Dictionary<object, object>)kvp.Value;
+					Assert.Equal("defgh", valueAsDict["anotherStringProperty"]);
+					Assert.Equal((byte)234, valueAsDict["anotherNumProperty"]);
+
+				},
+				kvp =>
+				{
+					//yaml deserializer loads simple objects as key-value pairs
+					Assert.Equal("foo", kvp.Key);
+					Assert.Equal("this is a test!", kvp.Value);
+				});
+		}
+
+		[Fact]
 		public void Can_resolve_multilevel_inheritance()
 		{
 			//sanity check
 			Assert.True(_repository.TryGetByName("inherited-template-level2", out var template));
 
-			var templateWithInheritance = _inheritanceResolver.GetEffectiveTemplate(template);
+			var templateWithInheritanceResolved = _inheritanceResolver.GetEffectiveTemplate(template);
 
-			Assert.NotNull(templateWithInheritance); //sanity check
+			Assert.NotNull(templateWithInheritanceResolved); //sanity check
 
-			Assert.Collection(templateWithInheritance.Tags.OrderBy(x => x),
+			Assert.Collection(templateWithInheritanceResolved.Tags.OrderBy(x => x),
 				tag => Assert.Equal("tag1", tag),
 				tag => Assert.Equal("tag2", tag),
 				tag => Assert.Equal("tag3", tag),
 				tag => Assert.Equal("tag4", tag));
 
-			Assert.Collection(templateWithInheritance.Components.OrderBy(x => x.Key),
+			Assert.Collection(templateWithInheritanceResolved.Components.OrderBy(x => x.Key),
 				kvp =>
 				{
 					//yaml deserializer loads simple objects as key-value pairs
